d5fde3bc85767ba42b1ceca4d0227901846d5a97
grosualex <grosu.alex1995@gmail.com>
2017-04-28 10:47:48 +0300

    Libs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,6 +9,6 @@ public class Main {
         Notification.Instance.sendSms(512, UserType.STUDENT, MessageType.REQUEST_DENIED);
         Notification.Instance.sendSms(222, UserType.SECRETARY, MessageType.REQUEST_FINALISED);
         Notification.Instance.sendSms(312, UserType.SECRETARY, MessageType.REQUEST_RECEIVED);
-        // Notification.Instance.sendEmail(213, UserType.SECRETARY, MessageType.REQUEST_RECEIVED);
+        Notification.Instance.sendEmail(213, UserType.SECRETARY, MessageType.REQUEST_RECEIVED);
     }
 }
