e5d79dc2f3c97cd328acae3e533518156ab3bc99
AlenaHa <andreea.hardon@gmail.com>
2017-06-01 12:49:23 +0300

    - added new controller and updated the old one in order to send approval/denial notification (sms+email)

--- notification-service/src/main/java/org/notification/NotificationController.java
+++ notification-service/src/main/java/org/notification/NotificationController.java
@@ -1,26 +1,38 @@
 package org.notification;
 
-import org.json.HTTP;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * Created by AlenaHa on 25.05.2017.
- */
+
 @RestController
 public class NotificationController {
 
-    @RequestMapping(value = "/send-test-email")
-    public ResponseEntity<?> sendTestNotification(@RequestParam String to) {
+    @RequestMapping(value = "/send-approve-notification")
+    public ResponseEntity<?> sendApprovalNotification(@RequestParam String email, @RequestParam String phoneNumber) {
         // TODO: test me with: http://localhost:8777/notification/send-test-email?to=andreea.hardon@gmail.com
         try {
-            Notification.Instance.sendEmail(to, MessageType.REQUEST_RECEIVED);
+            Notification.Instance.sendEmail(email, MessageType.REQUEST_APPROVED);
+            Notification.Instance.sendSms(phoneNumber, MessageType.REQUEST_APPROVED);
             return new ResponseEntity<Object>(HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
         }
     }
+
+    @RequestMapping(value = "/send-denial-notification")
+    public ResponseEntity<?> sendDenialNotification(@RequestParam String email, @RequestParam String phoneNumber) {
+        // TODO: test me with: http://localhost:8777/notification/send-test-email?to=andreea.hardon@gmail.com
+        try {
+            Notification.Instance.sendEmail(email, MessageType.REQUEST_DENIED);
+            Notification.Instance.sendSms(phoneNumber, MessageType.REQUEST_DENIED);
+            return new ResponseEntity<Object>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<Object>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
 }
